71eb91f2ebbc4673686cb27937c5b2f1802670b8
Trinity <trinityc@hollywoodbets.net>
2026-05-04 15:00:20 +0200

    sprint 2b: use data annotations for validation and schema hints

---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [Required, MaxLength(60)]
+        public string Brand { get; set; } = null!;
+
+        [Required, MaxLength(60)]
+        public string Category { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        [MaxLength(2000)]
+        public string? Description { get; set; }
+
+        [MaxLength(255)]
+        public string? ImageFileName { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
--- Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net9.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d949bc67b1827287706bdb57f916f2bb82446b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3db84dc58b2a36fbf2595c9e832f394e7e63f3b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
